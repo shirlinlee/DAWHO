3605502db294418149279691cd6426155a6b1edb
rachelhuang <rachel@3wcreative.com.tw>
2020-05-14 11:54:19 +0800

    [new]開戶

--- src/assets/js/open.js
+++ src/assets/js/open.js
@@ -1,6 +1,49 @@
 import mode from './modules/mode';
 
-const media = window.matchMedia("(max-width: 768px)")
+const media = window.matchMedia("(max-width: 768px)");
+let detectResize = false;
+
+
+
+$(document).ready(function() {
+    cardSlick()
+    $(window).resize(function () {
+        cardSlick()
+    });
+});
+
+function cardSlick() {
+    $('#card-slider-slick').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 9999,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+        // $('#card-slider-slick').slick({
+        //     dots: true,
+        //     infinite: false,
+        //     slidesToShow: 1,
+        //     slidesToScroll: 1
+        // });
+ 
+      
+}
 
 new Vue({
     el: '#appOpen',
@@ -10,21 +53,24 @@ new Vue({
     mounted: function () {
         mode();
         this.$nextTick(() => {
-            this.cardSlick()
+            // $('#card-slider-slick').slick({
+            //     dots: true,
+            //     infinite: false,
+            //     speed: 200,
+            //     slidesToShow: 1,
+            //     mobileFirst: true,
+            //     responsive: [
+            //         {
+            //             breakpoint: 768,
+            //             settings: 'unslick'
+            //         }
+            //     ]
+            // });
             
         });
     },
     methods: {
-       cardSlick() {
-            if (media.matches) {
-                $('#card-slider-slick').slick({
-                    dots: true,
-                    infinite: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                });
-            }
-       }
+       
     }
 });
 

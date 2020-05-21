108f372bcb0b6ff6b6512966892b8a42dd490584
lee <shirlin@fofo.tw>
2020-05-21 18:09:06 +0800

    增加list

---
+++ src/assets/js/list.js
@@ -0,0 +1,24 @@
+import mode from './modules/mode';
+// import Vue from 'vue';
+new Vue({
+    el: '#list',
+    data: {
+        lev_lists: [
+            { name: '', html: 'lev_1-1' },
+            { name: '', html: 'lev_1-2' },
+            { name: '', html: 'lev_2-1' },
+            { name: '', html: 'lev_2-2' },
+            { name: '', html: 'lev_3-1' },
+            { name: '', html: 'lev_3-2' },
+            { name: '', html: 'lev_3-3' },
+            { name: '', html: 'lev_3-4' },
+            { name: '', html: 'lev_3-5' },
+            { name: '', html: 'lev_4-1' },
+            { name: '', html: 'lev_4-2' },
+            { name: '', html: 'lev_4-3' },
+            { name: '', html: 'lev_4-4' },
+            { name: '', html: 'lev_4-5' },
+        ],
+    },
+    mounted: function () {},
+});

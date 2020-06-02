6752dda51eac60abb3bd11302819cd2a497b2edf
lee <shirlin@fofo.tw>
2020-06-03 00:30:56 +0800

    6/5交件

--- src/assets/js/cardless.js
+++ src/assets/js/cardless.js
@@ -21,9 +21,25 @@
         if (!this.value) {
             $(this).removeClass('hint');
             $('p.money-note span.hint').hide();
+        } else {
+            $('p.money-note span.note').show();
         }
-       else {
-        $('p.money-note span.note').show();
-        }
+    });
+
+    $('.QA').on('click', '.title', function () {
+        $(this).siblings('.content').slideToggle();
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 })(jQuery);

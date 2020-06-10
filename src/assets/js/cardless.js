0a31f9e48b532eff944eb4250e530c16e95a9802
junxiang <junxiang.oriint@gmail.com>
2020-06-11 01:12:40 +0800

    [無卡提款] 反饋修改 : 0611

--- src/assets/js/cardless.js
+++ src/assets/js/cardless.js
@@ -4,12 +4,12 @@
         $('p.money-note span.hint').hide();
 
         $('p.money-note span.note').show();
-        // $('div.quick').show();
+        $('div.quick').show();
     });
     $('.money').on('focusout', function () {
         $('p.money-note span.note').hide();
-        // $('div.quick').hide();
-
+        $('div.quick').hide();
+        $('p.money-note span.hint').hide();
         var min = parseInt($(this).attr('min'), 10);
         if (this.value < min) {
             $(this).addClass('hint');
@@ -20,9 +20,9 @@
         }
         if (!this.value) {
             $(this).removeClass('hint');
-            $('p.money-note span.hint').hide();
+            // $('p.money-note span.hint').hide();
         } else {
-            $('p.money-note span.note').show();
+            // $('p.money-note span.note').show();
         }
     });
 

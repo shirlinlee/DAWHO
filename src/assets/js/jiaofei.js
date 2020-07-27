4b47af1d035fb542259a75c042755ca919d1ca55
junxiang <junxiang.oriint@gmail.com>
2020-07-28 01:01:13 +0800

    [繳費] pm_1-2, wip

---
+++ src/assets/js/jiaofei.js
@@ -0,0 +1,29 @@
+(function ($) {})(jQuery);
+
+import mode from './modules/mode';
+new Vue({
+    el: '#appJiaofei',
+    data: {
+        drawerTel: false,
+        drawerCommon: false,
+        drawerCommonCreate: false,
+    },
+    mounted: function () {
+        this.$nextTick(() => {
+            // 電信公司 - 選擇
+            $('#item-select > select').on('change', function (e) {
+                $('#item-tel').hide();
+                $('#item-mobile').hide();
+                switch ($(this).val()) {
+                    case '遠傳電信':
+                        $('#item-mobile').show();
+                        break;
+                    case '中華電信':
+                        $('#item-tel').show();
+                        break;
+                    default:
+                }
+            });
+        });
+    },
+});

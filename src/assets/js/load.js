ed0edbfc55ce98f9cfa0869a6e4024f0ca7dc24c
lee <shirlin@fofo.tw>
2020-07-30 23:58:13 +0800

    wip - home

---
+++ src/assets/js/load.js
@@ -0,0 +1,10 @@
+import mode from './modules/mode';
+// import Vue from 'vue';
+new Vue({
+    el: '#appLoad',
+    data: {},
+    mounted: function () {
+        mode();
+    },
+    methods: {},
+});

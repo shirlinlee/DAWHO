bd9699301ffc320c5b441f9ac907da673b7e302b
junxiang <junxiang.oriint@gmail.com>
2020-07-31 14:51:30 +0800

    [繳費] Update: include scripts.pug

--- src/assets/js/jiaofei.js
+++ src/assets/js/jiaofei.js
@@ -1,101 +1,102 @@
-(function ($) {})(jQuery);
-
-import mode from './modules/mode';
-new Vue({
-    el: '#appJiaofei',
-    data: {
-        drawerTel: false,
-        drawerCommon: false,
-        drawerCommonCreate: false,
-        drawerProcessCommonCreate: false,
-        drawerCreditCommon: false,
-        drawerCreditCommonCreate: false,
-        drawerCreditIDCard: false,
-        drawerCreditCommonCreate2: false,
-        drawerBankCode: false,
-        drawerCommonBankCode: false,
-        numTel: '',
-        numListLi: -1,
-        ccNumber: '',
-        ccName: '',
-        ccProcessName: '',
-        ccCreditNumber: '1234 － 5678 － 9011 － 1213',
-        ccCreditName: '我的金卡',
-        creditMoney: '',
-        searchValue: '',
-        searchCommonValue: '',
-        valueBank: '',
-        valueCommonBank: '004 台灣分行',
-        BankList: [],
-    },
-    methods: {
-        clickNumListLi(v) {
-            // $('.num-list li .num-list-container.swipeleft').index('.num-list li .num-list-container')
-            if ($('.num-list li .num-list-container.swipeleft').length == 0) {
-                if (this.numListLi == v) {
-                    this.numListLi = -1;
-                } else {
-                    this.numListLi = v;
-                }
-            }
+(function ($) {
+    new Vue({
+        el: '#appJiaofei',
+        data: {
+            drawerTel: false,
+            drawerCommon: false,
+            drawerCommonCreate: false,
+            drawerProcessCommonCreate: false,
+            drawerCreditCommon: false,
+            drawerCreditCommonCreate: false,
+            drawerCreditIDCard: false,
+            drawerCreditCommonCreate2: false,
+            drawerBankCode: false,
+            drawerCommonBankCode: false,
+            numTel: '',
+            numListLi: -1,
+            ccNumber: '',
+            ccName: '',
+            ccProcessName: '',
+            ccCreditNumber: '1234 － 5678 － 9011 － 1213',
+            ccCreditName: '我的金卡',
+            creditMoney: '',
+            searchValue: '',
+            searchCommonValue: '',
+            valueBank: '',
+            valueCommonBank: '004 台灣分行',
+            BankList: [],
         },
-        openEnv() {
-            this.isENVOpen = true;
-        },
-    },
-    mounted: function () {
-        this.BankList = _htmlBankList;
-        this.$nextTick(() => {
-            // 電信公司 - 選擇
-            $('#item-select > select').on('change', function (e) {
-                $('#item-tel').hide();
-                $('#item-mobile').hide();
-                switch ($(this).val()) {
-                    case '遠傳電信':
-                        $('#item-mobile').show();
-                        break;
-                    case '中華電信':
-                        $('#item-tel').show();
-                        break;
-                    default:
+        methods: {
+            clickNumListLi(v) {
+                // $('.num-list li .num-list-container.swipeleft').index('.num-list li .num-list-container')
+                if ($('.num-list li .num-list-container.swipeleft').length == 0) {
+                    if (this.numListLi == v) {
+                        this.numListLi = -1;
+                    } else {
+                        this.numListLi = v;
+                    }
                 }
-            });
-
-            var numListContainer = $('.num-list-container');
-            numListContainer
-                .hammer()
-                .bind('swipeleft', function (e) {
-                    numListContainer.removeClass('swipeleft');
-                    numListContainer
-                        .siblings('.num-list-delete')
-                        .removeClass('swipeleft');
-
-                    $(this).addClass('swipeleft');
-                    setTimeout(() => {
-                        $(this)
-                            .siblings('.num-list-delete')
-                            .addClass('swipeleft');
-                    }, 300);
-                })
-                .bind('swiperight', function (e) {
-                    setTimeout(() => {
-                        $(this)
-                            .siblings('.num-list-delete')
-                            .removeClass('swipeleft');
-                    }, 0);
-                    $(this).removeClass('swipeleft');
+            },
+            openEnv() {
+                this.isENVOpen = true;
+            },
+        },
+        mounted: function () {
+            this.BankList = _htmlBankList;
+            this.$nextTick(() => {
+                // 電信公司 - 選擇
+                $('#item-select > select').on('change', function (e) {
+                    $('#item-tel').hide();
+                    $('#item-mobile').hide();
+                    switch ($(this).val()) {
+                        case '遠傳電信':
+                            $('#item-mobile').show();
+                            break;
+                        case '中華電信':
+                            $('#item-tel').show();
+                            break;
+                        default:
+                    }
                 });
 
-            var numListDelete = $('.num-list-delete');
-            numListDelete.on('click', function () {
-                $(this).parent().hide();
-            });
+                var numListContainer = $('.num-list-container');
+                numListContainer
+                    .hammer()
+                    .bind('swipeleft', function (e) {
+                        numListContainer.removeClass('swipeleft');
+                        numListContainer
+                            .siblings('.num-list-delete')
+                            .removeClass('swipeleft');
 
-            // 信用卡卡號
-            var cleave = new Cleave('.jf-credit-number', {
-                delimiter: ' － ',
-                blocks: [4, 4, 4, 4],
+                        $(this).addClass('swipeleft');
+                        setTimeout(() => {
+                            $(this)
+                                .siblings('.num-list-delete')
+                                .addClass('swipeleft');
+                        }, 300);
+                    })
+                    .bind('swiperight', function (e) {
+                        setTimeout(() => {
+                            $(this)
+                                .siblings('.num-list-delete')
+                                .removeClass('swipeleft');
+                        }, 0);
+                        $(this).removeClass('swipeleft');
+                    });
+
+                var numListDelete = $('.num-list-delete');
+                numListDelete.on('click', function () {
+                    $(this).parent().hide();
+                });
+
+                // 信用卡卡號
+                if ($('.jf-credit-number').length > 0)
+                    new Cleave('.jf-credit-number', {
+                        delimiter: ' － ',
+                        blocks: [4, 4, 4, 4],
+                    });
             });
-        });
-    },
-});
+        },
+    });
+})(jQuery);
+

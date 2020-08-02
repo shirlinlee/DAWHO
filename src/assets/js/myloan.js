5d539a43bb190b7ae0086a8904e0cf8b94b55ef3
lee <shirlin@fofo.tw>
2020-08-03 00:06:53 +0800

    update

--- src/assets/js/myloan.js
+++ src/assets/js/myloan.js
@@ -7,11 +7,37 @@ new Vue({
     },
     mounted: function () {
         mode();
-        this.$nextTick(() => {});
+        this.$nextTick(() => {
+            $('.list').on('click', 'li', function () {
+                const opened = $(this).hasClass('open');
+                $('.list li').removeClass('open').find('.detail').slideUp();
+                if (opened) return;
+
+                $(this).addClass('open').find('.detail').slideDown();
+            });
+
+            $('.tooltip_btn').tooltipster({
+                content: $('.tooltips_content'),
+                contentCloning: true,
+                animation: 'grow',
+                side: 'bottom',
+                arrow: false,
+                animationDuration: 450,
+                trigger: 'click',
+            });
+
+            $(window).on('scroll', function () {
+                $('.tooltip_btn').tooltipster('hide');
+            });
+
+            $('.cw-notice-title').on('click', function () {
+                $(this).toggleClass('active');
+                $('.cw-notice-content').slideToggle();
+            });
+        });
     },
     methods: {
         listHandler(type) {
-            console.log(type);
             this.listType = type;
         },
     },

bc3f917756f298f927c05be40abde52f2a5dbf1f
rachelhuang <rachel@3wcreative.com.tw>
2020-08-26 10:43:40 +0800

    [adjust] pdf加同意按鈕

--- src/assets/js/fancyLight.js
+++ src/assets/js/fancyLight.js
@@ -1,7 +1,7 @@
  
 (function ($) {
-  
-    $("a.iframe").fancybox({
+
+    const fancyOptions = {
         autoSize: true,
         type: 'iframe',
         toolbar: false,
@@ -11,11 +11,22 @@
         },
         //custom close 
         btnTpl: {
-            smallBtn: '<div data-fancybox-close class="fancybox-close-small modal-close">關閉</div>'
+            smallBtn: '<div data-fancybox-close class="fancybox-close-small modal-close">關閉</div>',
         }
-    });
+    }
+
+    // Create template for the button
+    $("a.iframe").fancybox(fancyOptions);
+
+    $("a.iframe-extraBtn").fancybox({
+        ...fancyOptions,
+        afterShow: function (opts, obj) {
+            const agreeBtnHtml = '<div data-fancybox-close class="modal-footer center"><span class="btn btn-primary">我已詳閱並同意</span></div>'
+            $('.fancybox-content').addClass('fancybox-content-agreeBtn').append(agreeBtnHtml);
+        }}
+    );
  
-    $("a.iframe.initOpen").trigger('click')
+    $("a.iframe.initOpen, a.iframe-extraBtn.initOpen").trigger('click')
   
     //http://jsfiddle.net/NUBru/
 })(jQuery);
